b7305ea9eed3148a0bfa764364551b38614b01d8
Swayam <swayamboob96@gmail.com>
2023-03-11 10:39:19 +0530

    contract check

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,6 +2,8 @@ import abi from './newTransaction.json';
 export const contractABI= abi.abi;
 // export const contractAddress= '0x11a2924dc31c414450817De2E5e08A4ab3820b08';
 // export const contractAddress= "0x97CA12f5eFBc2D41251F4Ef0918aC087eecd8472";
-export const contractAddress="0x8AD6987C07c6C336E68af71FE8314e7d41516cB9";
+// export const contractAddress="0x8AD6987C07c6C336E68af71FE8314e7d41516cB9";
+
+export const contractAddress="0x913F3016ca8E4bBAf6A1459b67e83119FaC76638";
 // export const contractAddress="0xc24028a7ad8e79978789a1b9c775cf71dee54fa1d68b1ce017a552baa20af8ed";
 
